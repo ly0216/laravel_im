e6bb8565a9a82a2e461ad0e92a5da87ea7c82459
ly0216 <1005305159@qq.com>
2021-06-03 20:46:03 +0800

    路由

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/liy',function(){
-    return "Hello word!";
-})
+Route::get('liy',function(){
+    dd("Hello word!");
+});
